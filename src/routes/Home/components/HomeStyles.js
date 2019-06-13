c3270db5deb0cd9bd54270331393e868b3fa9667
felipe <ethszx@gmail.com>
2019-06-13 14:27:28 -0300

    added home styles

---
+++ src/routes/Home/components/HomeStyles.js
@@ -0,0 +1,15 @@
+const styles = theme => ({
+
+    layout: {
+        width: 'auto',
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+          width: 900,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        },
+    }
+});
+
+export default styles;
