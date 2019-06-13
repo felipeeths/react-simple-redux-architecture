696a9116afbf5837c13cda7e23c689f57ab4616e
felipe <ethszx@gmail.com>
2019-06-13 14:05:53 -0300

    fix app file

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import { Route, Switch } from 'react-router' // react-router v4/v5
 import HomeContainer from "./routes/Home/containers/HomeContainer";
 import './App.css';
 
-function App() {
-	return (
+class App extends Component {
+	static propTypes = {
+		cookies: instanceOf(Cookies).isRequired
+	};
 
-		<div>
-			<Switch>
-				<Route path='/' component={HomeContainer} />
-			</Switch>
-		</div>
+	render() {
+			return (
 
-	);
+			<div>
+				<Switch>
+					<Route path='/' component={HomeContainer} />
+				</Switch>
+			</div>
+
+		);
+	}
 }
 
 export default App;
