a52512c05929fe12ca786d6df17727d65e81f36c
felipe <ethszx@gmail.com>
2019-06-13 13:20:23 -0300

    fix architeture and package

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import configureStore,  { history } from './store/configureStore';
 import { Route, Switch } from 'react-router' // react-router v4/v5
 import HomeContainer from "./routes/Home/containers/HomeContainer";
-import { ConnectedRouter } from 'connected-react-router'
-import logo from './logo.svg';
 import './App.css';
 
-const store = configureStore();
-
 function App() {
-  return (
-    <Provider store={store}>
-       <ConnectedRouter history={history}>
-	   		<div>
-			   <Switch>
-			   		<Route path='/' component={HomeContainer} />
-			   </Switch>
-			</div>
-       </ConnectedRouter>
-	</Provider>
-  );
+	return (
+
+		<div>
+			<Switch>
+				<Route path='/' component={HomeContainer} />
+			</Switch>
+		</div>
+
+	);
 }
 
 export default App;
